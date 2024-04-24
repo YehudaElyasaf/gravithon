e94d091ed52596a7e8ef714a9a1debed6e9bf036
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2024-04-24 03:45:44 +0300

    fix ss example

--- gravithon/example/SolarSystem.py
+++ gravithon/example/SolarSystem.py
@@ -11,26 +11,23 @@ class SolarSystem(Space):
     def __init__(self):
         super().__init__(DIMENSIONS)
 
-        # Create space
-        self.space = Space(dimensions=DIMENSIONS, fps=100)
-
         # Add sun
-        self.space.add_body(Sun, array([130000000000, 0]))
+        self.add_body(Sun, array([130000000000, 0]))
         # Add planets
-        self.space.add_body(Mercury, Sun)
-        self.space.add_body(Venus, Sun)
-        self.space.add_body(Earth, Sun)
-        self.space.add_body(Mars, Sun)
-        self.space.add_body(Jupiter, Sun)
-        self.space.add_body(Saturn, Sun)
-        self.space.add_body(Uranus, Sun)
-        self.space.add_body(Pluto, Sun)
+        self.add_body(Mercury, Sun)
+        self.add_body(Venus, Sun)
+        self.add_body(Earth, Sun)
+        self.add_body(Mars, Sun)
+        self.add_body(Jupiter, Sun)
+        self.add_body(Saturn, Sun)
+        self.add_body(Uranus, Sun)
+        self.add_body(Pluto, Sun)
         # Add moon
-        self.space.add_body(Moon, Earth)
+        self.add_body(Moon, Earth)
 
     def run(self):
-        print(self.space)
-        Screen(self.space, speed=1000000).show()
+        print(self)
+        Screen(self, speed=1000000).show()
 
 
 if __name__ == '__main__':
